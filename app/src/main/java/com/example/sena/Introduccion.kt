77a57cf1cf83fa42adc01e2cd9bd2103bfa7d300
gabysssspam <gongoramendezfernandagabriellemail.com>
2025-10-26 23:26:04 -0600

    26/10/25 4ta actualización hecha por Gabrielle Góngora

--- app/src/main/java/com/example/sena/Introduccion.kt
+++ app/src/main/java/com/example/sena/Introduccion.kt
@@ -1,5 +1,6 @@
 package com.example.sena
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Introduccion : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
